259ee879d411e5890651de4ab3be708b68a51ce1
skybohan <51256499+skybohan@users.noreply.github.com>
2019-07-27 11:01:51 -0500

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,11 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : 'Add SSID here',             # Keep the two '' quotes around the name
+    'password' : 'Add WiFi password here',         # Keep the two '' quotes around password
+    'timezone' : 'Add your timezone here',  # http://worldtimeapi.org/timezones
+    'darksky_token' : 'Add API key here' # Requires free Dark Sky developer account from https://darksky.net/dev 
+    }
